a113b66d3f8038ef24003004b3a4b541c4c72469
Lucas <lucasemanuelribeiro@hotmail.com>
2024-04-10 10:40:58 -0300

    Aplicando reconheciemento por voz

--- index.py
+++ index.py
@@ -2,7 +2,16 @@ import speech_recognition as sr
 
 mic = sr.Recognizer()
 
-with sr.Microphone as source:
+with sr.Microphone() as source:
     mic.adjust_for_ambient_noise(source)
 
-    print('Vamos começar, fale alguma coisa')
+    print('Vamos começar, fale alguma coisa..')
+
+    audio = mic.listen(source)
+
+    try:
+        frase = mic.recognize_google(audio, language = 'pt-BR')
+        print('Você falou: '+frase)
+
+    except sr.UnknownValueError:
+        print('Ops.. Algo deu errado.')
